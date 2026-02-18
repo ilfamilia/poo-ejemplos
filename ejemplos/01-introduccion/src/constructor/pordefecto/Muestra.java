03dee608861bc6e320a5d017cd3989c26181c8f1
Israel Liranzo <45414112+ilfamilia@users.noreply.github.com>
2026-02-18 13:20:41 -0400

    Agregar ejemplo de constructor por defecto

---
+++ ejemplos/01-introduccion/src/constructor/pordefecto/Muestra.java
@@ -0,0 +1,9 @@
+package constructor.pordefecto;
+
+// Clase creada sin constructor para mostrar como los campos se inicializan a traves
+// del constructor por defecto
+class Muestra {
+    int i;
+    String s;
+    boolean b;
+}
