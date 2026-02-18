653cc5763547539cb0d8a2db38668449be33931e
Israel Liranzo <45414112+ilfamilia@users.noreply.github.com>
2026-02-18 15:21:48 -0400

    Agregar ejemplo de constructor con argumentos

---
+++ ejemplos/01-introduccion/src/constructor/conargumentos/Pagina.java
@@ -0,0 +1,9 @@
+package constructor.conargumentos;
+
+class Pagina {
+
+    // Ejemplo de constructor con argumentos o parámetros
+    Pagina(int i){
+        System.out.print("pag. " + i + " ");
+    }
+}
