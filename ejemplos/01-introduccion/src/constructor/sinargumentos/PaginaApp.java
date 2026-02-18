b6dfb4674b23c44c17eca357d1f6b5de1124328d
Israel Liranzo <45414112+ilfamilia@users.noreply.github.com>
2026-02-18 13:39:15 -0400

    Agregar ejemplo de constructor sin argumentos

---
+++ ejemplos/01-introduccion/src/constructor/sinargumentos/PaginaApp.java
@@ -0,0 +1,10 @@
+package constructor.sinargumentos;
+
+public class PaginaApp {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            // Cuando hay un constructor en la clase el constructor por defecto no es utilizado
+            new Pagina();
+        }
+    }
+}
