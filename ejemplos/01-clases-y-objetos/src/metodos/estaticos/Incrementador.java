eeff5c767d546b7926890987b66e61b0959ffb82
Israel Liranzo <45414112+ilfamilia@users.noreply.github.com>
2026-02-19 09:34:53 -0400

    Agregar comentario explicativo sobre crear métodos no estáticos dentro de métodos estáticos

--- ejemplos/01-clases-y-objetos/src/metodos/estaticos/Incrementador.java
+++ ejemplos/01-clases-y-objetos/src/metodos/estaticos/Incrementador.java
@@ -3,6 +3,7 @@ package metodos.estaticos;
 public class Incrementador {
 
     // Creación de metodo estático dentro de la clase
+    // Ojo: no se puede llamar metodos no estáticos dentro de metodos estáticos
     static void incrementar() {
         PruebaStatic.i++;
     }
