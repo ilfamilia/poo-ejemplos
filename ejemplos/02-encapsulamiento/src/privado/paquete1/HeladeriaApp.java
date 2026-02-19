7629740f7b7b48cdb9e6375a274afeb1a0eb548d
Israel Liranzo <45414112+ilfamilia@users.noreply.github.com>
2026-02-19 15:57:23 -0400

    Agregar ejemplo de acceso de tipo privado

---
+++ ejemplos/02-encapsulamiento/src/privado/paquete1/HeladeriaApp.java
@@ -0,0 +1,10 @@
+package privado.paquete1;
+
+// Esta clase no tiene acceso al constructor de la clase Helado porque es privado
+public class HeladeriaApp {
+    public static void main(String[] args) {
+        //! Helado helado = new Helado();
+
+        Helado helado = Helado.hacerHelado();
+    }
+}
