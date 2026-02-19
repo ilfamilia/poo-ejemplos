57ec593fed23a956b9961fa251fdd3758cdb302d
Israel Liranzo <45414112+ilfamilia@users.noreply.github.com>
2026-02-19 04:09:01 -0400

    Agregar ejemplo de this para referirse a un miembro de la clase

---
+++ ejemplos/01-clases-y-objetos/src/referenciathis/comomiembro/Basquetbolista.java
@@ -0,0 +1,21 @@
+package referenciathis.comomiembro;
+
+class Basquetbolista {
+    String nombre;
+    String posicion;
+    int numero;
+
+    Basquetbolista(String nombre, String posicion, int numero) {
+        // Este ejemplo muestra como this puede ser usado para resolver la ambiguedad
+        // entre los datos miembros de la clase y la lista de argumentos del constructor
+        this.nombre = nombre;
+        this.posicion = posicion;
+        this.numero = numero;
+    }
+
+    void mostrarDatos() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Posición: " + posicion);
+        System.out.println("Número: " + numero);
+    }
+}
---
+++ ejemplos/01-clases-y-objetos/src/referenciathis/comomiembro/BasquetbolistaApp.java
@@ -0,0 +1,9 @@
+package referenciathis.comomiembro;
+
+public class BasquetbolistaApp {
+    public static void main(String[] args) {
+        Basquetbolista b;
+        b = new Basquetbolista("Stephen Curry", "base", 30);
+        b.mostrarDatos();
+    }
+}
