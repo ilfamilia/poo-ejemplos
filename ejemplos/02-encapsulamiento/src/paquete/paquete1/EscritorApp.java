b5fa7e1d816a786df46eef17ba3d7c8cc44758b3
Israel Liranzo <45414112+ilfamilia@users.noreply.github.com>
2026-02-19 13:18:57 -0400

    Agregar ejemplo de acceso de tipo paquete

---
+++ ejemplos/02-encapsulamiento/src/paquete/paquete1/EscritorApp.java
@@ -0,0 +1,12 @@
+package paquete.paquete1;
+
+public class EscritorApp {
+    public static void main(String[] args) {
+        Escritor escritor = new Escritor();
+        escritor.escribir("Prueba 1");
+        escritor.escribir("Prueba 2");
+        escritor.escribir("Prueba 3");
+        escritor.escribir("Prueba 4");
+        escritor.escribir("Prueba 5");
+    }
+}
