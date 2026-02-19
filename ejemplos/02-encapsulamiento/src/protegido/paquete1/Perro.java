1f0553a9fc9fae605435dfbafa933cfe414bb27c
Israel Liranzo <45414112+ilfamilia@users.noreply.github.com>
2026-02-19 17:05:57 -0400

    Agregar ejemplo de acceso de tipo protegido

---
+++ ejemplos/02-encapsulamiento/src/protegido/paquete1/Perro.java
@@ -0,0 +1,15 @@
+package protegido.paquete1;
+
+// Esta clase muestra que las clases hijas pueden acceder a los miembros
+// de tipo protected de la clase padre
+public class Perro extends Animal {
+    public Perro() {
+        System.out.println("Constructor del perro");
+    }
+
+    // Como esta clase hereda de Animal puede usar su metodo morder
+    public void mordisquear() {
+        System.out.print("Perro: ");
+        morder();
+    }
+}
---
+++ ejemplos/02-encapsulamiento/src/protegido/paquete2/AnimalApp2.java
@@ -0,0 +1,14 @@
+package protegido.paquete2;
+
+import protegido.paquete1.Animal;
+
+public class AnimalApp2 {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        gato.mordisquear();
+
+        // Esta clase no puede acceder al metodo morder porque no hereda de Animal
+        // ni pertenece al mismo paquete
+        //! new Animal().morder();
+    }
+}
