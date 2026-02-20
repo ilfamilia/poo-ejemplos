b10848bbcac9f3dc5a42f71f82c489cccf3932b4
Israel Liranzo <45414112+ilfamilia@users.noreply.github.com>
2026-02-20 14:28:03 -0400

    Agregar ejemplo sobre uso del polimorfismo de inclusión

---
+++ ejemplos/04-polimorfismo/src/inclusion/sintaxis/Instrumento.java
@@ -0,0 +1,7 @@
+package inclusion.sintaxis;
+
+class Instrumento {
+    public void tocar(String nota) {
+        System.out.println("Instrumento toca nota: " + nota);
+    }
+}
---
+++ ejemplos/04-polimorfismo/src/inclusion/sintaxis/MusicaApp.java
@@ -0,0 +1,15 @@
+package inclusion.sintaxis;
+
+// Se crea un metodo tipo estático que recibe un objeto de tipo Instrumento
+// como parámetro. Lo que significa que también podemos pasar como parámetro
+// cualquier otro objeto que herede de Instrumento
+public class MusicaApp {
+
+    public static void sonar(Instrumento i) {
+        i.tocar("DO");
+    }
+    public static void main(String[] args) {
+        Cuerda guitarra = new Cuerda();
+        sonar(guitarra); // Upcasting
+    }
+}
