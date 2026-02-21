1abf27cf51bf97060a0c13ece90039fe3eb629e8
Israel Liranzo <45414112+ilfamilia@users.noreply.github.com>
2026-02-20 23:11:17 -0400

    Mejorar comentarios de ejemplo de constructor sin parametros

--- ejemplos/01-clases-y-objetos/src/constructor/sin_argumentos/PaginaApp.java
+++ ejemplos/01-clases-y-objetos/src/constructor/sin_argumentos/PaginaApp.java
@@ -3,7 +3,8 @@ package constructor.sin_argumentos;
 public class PaginaApp {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            // Cuando hay un constructor en la clase el constructor por defecto no es utilizado
+            // Cuando la clase define un constructor, el compilador ya no genera automáticamente
+            // el constructor por defecto.
             new Pagina();
         }
     }
